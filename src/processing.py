155f4c4f378852121925dfd88b803f2dcf04097a
Ekaterina <katyat1@mail.ru>
2024-09-13 19:02:18 +1000

    исправлены ошибки flake, mypy,black,isort

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,18 @@
 from typing import Any
 
 states_dict = [
-{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
-def filter_by_state (states: list[dict[str, Any]], state_id: str = 'EXECUTED') -> list[dict[str, Any]]:
+
+def filter_by_state(states: list[dict[str, Any]], state_id: str = "EXECUTED") -> list[dict[str, Any]]:
     """Функция фильтрации операций по ключу"""
     states_list = []
     for key in states:
-        if key.get('state') == state_id:
+        if key.get("state") == state_id:
             states_list.append(key)
     return states_list
 
@@ -19,10 +20,10 @@ def filter_by_state (states: list[dict[str, Any]], state_id: str = 'EXECUTED') -
 print(filter_by_state(states_dict))
 
 
-def sort_by_date (states_dict: list[dict[str, Any]], revers:bool = True ) -> list[dict[str, Any]]:
+def sort_by_date(states_list: list[dict[str, Any]], revers: bool = True) -> list[dict[str, Any]]:
     """Функция сортировки по дате"""
-    sorted_list = sorted(states_dict, key=lambda x:x['date'], reverse=revers)
+    sorted_list = sorted(states_list, key=lambda x: x["date"], reverse=revers)
     return sorted_list
 
-print(sort_by_date(states_dict))
 
+print(sort_by_date(states_dict))
