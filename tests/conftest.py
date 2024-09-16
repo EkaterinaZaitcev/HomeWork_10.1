a10038c9bebaeb77964a90ddeec5613046d3032a
Ekaterina <katyat1@mail.ru>
2024-09-17 00:09:06 +1000

    написан test_masks и test_widget и conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,12 @@ import pytest
 
 @pytest.fixture
 def test_get_mask_account_fix():
-    return '**4305'
+    return '73654108430135874305'
 
 @pytest.fixture
-def mask_account_card_fix():
-    return 'Счет 64686473678894779589','Visa Platinum 7000792289606361', 'Счет 35383033474447895560'
-
-
+def test_get_date_fix():
+    return "2024-03-11T02:26:18.671407"
 
 @pytest.fixture
-def get_date_fix():
-    return '2024-03-11T02:26:18.671407','2025-12-11T02:26:18.671407', '2024-03-11T02:2', '2024-03-11T0255555555:26:18.671407', '6651665', '', 'hgyjd462'
+def test_mask_account_card_fix():
+    return "Счет 64686473678894779589"
