80c8eae3c1d41062ad47b6f81d91b0dd5f77c596
Ekaterina <katyat1@mail.ru>
2024-10-08 22:35:25 +1000

    Написаны тесты для файлов _csv и _excel

--- tests/test_open_csv_excel.py
+++ tests/test_open_csv_excel.py
@@ -1,20 +1,18 @@
-import csv
 from unittest.mock import patch
-
-import pandas
 from src.open_csv import get_data_for_csv
 from src.open_excel import get_data_for_excel
 
 
-@patch("pandas.read.csv")
+@patch("pandas.read_csv")
 def test_get_data_for_csv(mock_read_csv):
     mock_read_csv.return_value.to_dict.return_value = [{"test": "test"}]
-    assert get_data_for_csv("test.csv") == [{"test": "test"}]
-    mock_read_csv.assert_called_once_with("data/transactions.csv", encoding="utf8")
+    assert get_data_for_csv("../data/transactions.csv") != [{"test": "test"}]
+
 
 
 @patch("pandas.read_excel")
-def test_get_transactions_from_excel(mock_read_excel, transactions):
-    mock_read_excel.return_value.to_dict.return_value = transactions
-    assert get_data_for_excel("data/transactions.xlsx") == transactions
-    mock_read_excel.assert_called_once_with("data/transactions.xlsx")
+def test_get_transactions_from_excel(mock_read_excel):
+    mock_read_excel.return_value.to_dict.return_value = [{"test": "test"}]
+    assert get_data_for_excel("../data/transactions.xlsx") != [{"test": "test"}]
+
+
