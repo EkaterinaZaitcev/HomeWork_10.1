7ee430b1a99663edca7bfbf563427cf9296d2d15
Ekaterina <katyat1@mail.ru>
2024-10-11 14:54:39 +1000

    Создание модуля main и функции main

--- src/transactions.py
+++ src/transactions.py
@@ -1,5 +1,8 @@
 import json
+import re
+from typing import Dict, List
 
+from src.utils import get_data_from_csv
 from tests.test_generators import transactions
 
 
@@ -10,3 +13,17 @@ def filter_by_currency(currency):
     for transaction in data:
         if transaction["currency"] == currency:
             yield transaction
+
+
+def sorting_transactions(search_string: str) -> List[Dict]:
+    """Функция для поиска операций по заданной строке"""
+    transactions_csv = get_data_from_csv()
+    pattern = rf"{search_string}"
+    result_transactions = [transactions
+                           for transaction in transactions_csv
+                           if re.findall(pattern, transaction["description"], flags=re.IGNORECASE)]
+    return result_transactions
+
+if __name__ == "__mane__":
+    result = sorting_transactions()
+    print(result)
